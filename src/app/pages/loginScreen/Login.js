8c32d05f07e319e260ca0332d990b3bd48605455
anandnk03 <anandnk03@gmail.com>
2023-04-12 21:43:24 +0530

    login ui complete

--- src/app/pages/loginScreen/Login.js
+++ src/app/pages/loginScreen/Login.js
@@ -1,45 +1,53 @@
 import React from 'react';
+import LoginLog from '../../assets/img/ezgif.com-gif-maker-8.gif';
+import LoginBackground from '../../assets/img/loginscreenBackground.png';
 
 const Login = () => {
   return (
     <>
-      <div className="login-screen">
+      <div
+        className="login-screen"
+        style={{ backgroundImage: `url(${LoginBackground})` }}>
         <div className="container">
           <div className="col-sm-12">
-            <div className="row">
-              <div className="card card-wrap">
-                <div className="col-sm-6">
-                  <div className="title">
-                    <h4>Welcome</h4>
+            <div className="card card-wrap">
+              <div className="row">
+                <div className="col-sm-6 ">
+                  <h4 className="welcome-text">Welcome</h4>
+                  <div className="form-container">
+                    <form>
+                      <div className="form-group">
+                        <span>User Name</span>
+                        <input
+                          className="form-control"
+                          name=""
+                          type="text"
+                          placeholder="User Name"
+                        />
+                      </div>
+                      <div className="form-group">
+                        <span>Password</span>
+                        <input
+                          className="form-control"
+                          name=""
+                          type="password"
+                          placeholder="Password"
+                        />
+                      </div>
+                      <div className="button-group">
+                        <button type="button" className="btn btn-light">
+                          Login
+                        </button>
+                      </div>
+                    </form>
                   </div>
-                  <form>
-                    <div className="col-6">
-                      <span>User Name</span>
-                      <input
-                        className="form-control input-from"
-                        name=""
-                        type="text"
-                        placeholder="User Name"
-                      />
-                    </div>
-                    <div className="col-6">
-                      <span>Password</span>
-                      <input
-                        className="form-control input-from"
-                        name=""
-                        type="password"
-                        placeholder="Password"
-                      />
-                    </div>
-                    <div className="login-button">
-                      <button type="button" className="btn btn-light">
-                        Login{' '}
-                      </button>
-                    </div>
-                  </form>
                 </div>
                 <div className="col-sm-6">
-                  <h4>images</h4>
+                  <img
+                    src={LoginLog}
+                    className="login-images"
+                    alt="login-image"
+                  />
                 </div>
               </div>
             </div>
