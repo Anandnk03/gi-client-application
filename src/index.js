c715b221f1ef0b7891fd67e19facb2641d289366
anandnk03 <anandnk03@gmail.com>
2023-05-25 13:09:09 +0530

    done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/redux';
-import App from './app';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
